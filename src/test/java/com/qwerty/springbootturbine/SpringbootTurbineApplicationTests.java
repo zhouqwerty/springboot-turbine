6d1322ae19f0b607a38beaf66b992478cc5a305b
zhou <2891128968@qq.com>
2019-06-06 00:05:33 +0800

    Initial commit

---
+++ src/test/java/com/qwerty/springbootturbine/SpringbootTurbineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qwerty.springbootturbine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
